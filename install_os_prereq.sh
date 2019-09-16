3d5d136cfbc760dcb596a89d5490b1e4d90f8bd9
Ryan Koehler <rjkoehl@gmail.com>
2019-09-16 19:38:39 -0400

    Update install_os_prereq.sh

--- install_os_prereq.sh
+++ install_os_prereq.sh
@@ -9,4 +9,6 @@ apt-get upgrade
 
 sudo apt install git python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx curl python3-venv virtualenv supervisor
 
+sudo pip3 install virtualenv 
+
 echo "All required packages have been installed!"
